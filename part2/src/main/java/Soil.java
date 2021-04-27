a11fa8c223977ddbd97debd5dfb93a5f9fbb4b4c
Makoto <makoto@emurasoft.com>
2021-04-26 22:11:53 -0700

    readFile() and Grid

--- part2/src/main/java/Soil.java
+++ part2/src/main/java/Soil.java
@@ -1,22 +1,55 @@
-public class Soil {
-	public static boolean drains(int width, Pair[] connections) {
-		// id 0 = top; id 1 = bottom
+import org.jetbrains.annotations.Nullable;
 
-		WeightedPathCompression graph = new WeightedPathCompression(width * width + 2);
-		for (int i = 0; i < width; i++) {
-			graph.union(coordinate_to_id(width, i, 0), 0);
-			graph.union(coordinate_to_id(width, i, width - 1), 1);
+import java.util.*;
+
+public class Soil {
+	public static Grid readFile(Readable file) {
+		var scanner = new Scanner(file);
+		var squares = new Vector<Square>();
+
+		int width = 0;
+
+		int row = 0;
+		while (scanner.hasNext()) {
+			var str = scanner.next();
+			if (row == 0) {
+				width = str.replaceAll("\\s", "").length();
+			}
+
+			for (int i = 0; i < width; i++) {
+				switch (str.charAt(i)) {
+					case '0' -> squares.add(Square.Hold);
+					case '1' -> squares.add(Square.Drain);
+					case ' ' | '\t' -> {}
+					default -> throw new RuntimeException("invalid character");
+				}
+			}
+
+			row++;
 		}
 
-		for (Pair pair : connections) {
+		return new Grid(squares.toArray(Square[]::new), width);
+	}
+
+	public static boolean drains(Grid grid) {
+		var topID = grid.width() * grid.width();
+		var bottomID = topID + 1;
+
+		var graph = new WeightedPathCompression(grid.width() * grid.width() + 2);
+		for (int i = 0; i < grid.width(); i++) {
+			graph.union(coordinate_to_id(grid.width(), i, 0), topID);
+			graph.union(coordinate_to_id(grid.width(), i, grid.width() - 1), bottomID);
+		}
+
+		for (var pair : grid) {
 			graph.union(pair.a, pair.b);
 		}
 
-		return graph.isConnected(0, 1);
+		return graph.isConnected(topID, bottomID);
 	}
 
 	public static int coordinate_to_id(int width, int x, int y) {
-		return y * width + x + 2;
+		return y * width + x;
 	}
 }
 
@@ -27,5 +60,124 @@ class Pair {
 	public Pair(int a, int b) {
 		this.a = a;
 		this.b = b;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Pair pair = (Pair) o;
+		return a == pair.a && b == pair.b;
+	}
+
+	@Override
+	public String toString() {
+		return "Pair{" +
+			"a=" + a +
+			", b=" + b +
+			'}';
+	}
+}
+
+enum Square {
+	Hold,
+	Drain,
+}
+
+class Grid implements Iterable<Pair> {
+	public Grid(Square[] data, int width) {
+		this.data = data;
+		this.width = width;
+	}
+
+	public int width() {
+		return width;
+	}
+
+	@Override
+	public Iterator<Pair> iterator() {
+		return new GridIterator(this);
+	}
+
+	protected final Square[] data;
+	private final int width;
+
+	static class GridIterator implements Iterator<Pair> {
+		GridIterator(Grid grid) {
+			this.grid = grid;
+			this.index = 0;
+			this.vertical = false;
+			this.next = null;
+		}
+
+		@Override
+		public boolean hasNext() {
+			// Iterate through horizontal pairs
+			if (!vertical) {
+				if (grid.width == 0) {
+					return false;
+				}
+
+				while (true) {
+					if (index % grid.width == grid.width - 1) {
+						// Skip extra index at edges
+						index += 2;
+					} else {
+						index++;
+					}
+
+					if (index >= grid.width * grid.width) {
+						index = 0;
+						vertical = true;
+						break;
+					}
+
+					if (
+						grid.data[index - 1] == Square.Drain
+							&& grid.data[index] == Square.Drain
+					) {
+						this.next = new Pair(index - 1, index);
+						return true;
+					}
+				}
+			}
+
+			// Vertical pairs
+			while (true) {
+				if (index >= grid.width * (grid.width - 1)) {
+					this.next = null;
+					return false;
+				} else {
+					index++;
+				}
+
+				if (
+					grid.data[index - 1] == Square.Drain
+						&& grid.data[(index - 1) + grid.width] == Square.Drain
+				) {
+					this.next = new Pair(index - 1, (index - 1) + grid.width);
+					return true;
+				}
+			}
+		}
+
+		@Override
+		public Pair next() {
+			if (next == null) {
+				throw new NoSuchElementException();
+			}
+
+			return next;
+		}
+
+		private final Grid grid;
+		private int index;
+		private boolean vertical;
+		@Nullable
+		private Pair next;
 	}
 }
--- part2/src/test/java/SoilTest.java
+++ part2/src/test/java/SoilTest.java
@@ -3,71 +3,105 @@ import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.io.StringReader;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Vector;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Enclosed.class)
 public class SoilTest {
+	@RunWith(Parameterized.class)
+	public static class readFile {
+		@Parameterized.Parameters
+		public static Collection<Object> data() {
+			return Arrays.asList(new Object[][]{
+				{
+					"",
+					new Square[]{},
+				},
+				{
+					"0",
+					new Square[]{Square.Hold},
+				},
+				{
+					"1",
+					new Square[]{Square.Drain},
+				},
+				{
+					"10\n01",
+					new Square[]{Square.Drain, Square.Hold, Square.Hold, Square.Drain},
+				},
+				{
+					"1 0\n0 1",
+					new Square[]{Square.Drain, Square.Hold, Square.Hold, Square.Drain},
+				},
+			});
+		}
+
+		@Parameterized.Parameter
+		public String text;
+
+		@Parameterized.Parameter(1)
+		public Square[] expected;
+
+		@Test
+		public void test() {
+			assertArrayEquals(expected, Soil.readFile(new StringReader(text)).data);
+		}
+	}
 
 	@RunWith(Parameterized.class)
 	public static class drains {
-
 		@Parameterized.Parameters
 		public static Collection<Object> data() {
 			return Arrays.asList(new Object[][] {
 				{ // 0
-					0,
-					new Pair[]{},
+					new Square[][]{},
 					false,
 				},
 				{ // 1
-					1,
-					new Pair[]{},
+					new Square[][]{
+						{Square.Hold},
+					},
 					true,
 				},
 				{ // 2
-					2,
-					new Pair[]{},
+					new Square[][]{
+						{Square.Hold, Square.Hold},
+						{Square.Hold, Square.Hold},
+					},
 					false,
 				},
 				{ // 3
-					2,
-					new Pair[]{
-						new Pair(
-							Soil.coordinate_to_id(2, 0, 0),
-							Soil.coordinate_to_id(2, 1, 0)
-						),
+					new Square[][]{
+						{Square.Drain, Square.Drain},
+						{Square.Hold, Square.Hold},
 					},
 					false,
 				},
 				{ // 4
-					2,
-					new Pair[]{
-						new Pair(
-							Soil.coordinate_to_id(2, 0, 0),
-							Soil.coordinate_to_id(2, 0, 1)
-						),
+					new Square[][]{
+						{Square.Drain, Square.Hold},
+						{Square.Drain, Square.Hold},
 					},
 					true,
 				},
 				{ // 5
-					3,
-					new Pair[]{},
+					new Square[][]{
+						{Square.Hold, Square.Hold, Square.Hold},
+						{Square.Hold, Square.Hold, Square.Hold},
+						{Square.Hold, Square.Hold, Square.Hold},
+					},
 					false,
 				},
 				{ // 6
-					3,
-					new Pair[]{
-						new Pair(
-							Soil.coordinate_to_id(3, 0, 0),
-							Soil.coordinate_to_id(3, 0, 1)
-						),
-						new Pair(
-							Soil.coordinate_to_id(3, 0, 1),
-							Soil.coordinate_to_id(3, 0, 2)
-						),
+					new Square[][]{
+						{Square.Drain, Square.Drain, Square.Hold},
+						{Square.Drain, Square.Hold, Square.Hold},
+						{Square.Drain, Square.Hold, Square.Hold},
 					},
 					true,
 				},
@@ -75,17 +109,120 @@ public class SoilTest {
 		}
 
 		@Parameterized.Parameter
-		public int width;
+		public Square[][] squares;
 
 		@Parameterized.Parameter(1)
-		public Pair[] connections;
-
-		@Parameterized.Parameter(2)
 		public boolean expected;
 
 		@Test
 		public void test() {
-			assertEquals(expected, Soil.drains(width, connections));
+			Square[] flattened = Arrays.stream(squares)
+				.flatMap(Arrays::stream)
+				.toArray(Square[]::new);
+
+			assertEquals(expected, Soil.drains(new Grid(flattened, squares.length)));
+		}
+	}
+
+	@RunWith(Parameterized.class)
+	public static class GridTest {
+		@Parameterized.Parameters
+		public static Collection<Object> data() {
+			return Arrays.asList(new Object[][]{
+				{
+					new Square[][] {},
+					new Pair[] {},
+				},
+				{
+					new Square[][] {
+						{Square.Drain},
+					},
+					new Pair[] {},
+				},
+				{
+					new Square[][] {
+						{Square.Hold, Square.Hold},
+						{Square.Hold, Square.Hold},
+					},
+					new Pair[] {},
+				},
+				{
+					new Square[][] {
+						{Square.Drain, Square.Hold},
+						{Square.Hold, Square.Hold},
+					},
+					new Pair[] {},
+				},
+				{
+					new Square[][] {
+						{Square.Drain, Square.Hold},
+						{Square.Hold, Square.Drain},
+					},
+					new Pair[] {},
+				},
+				{
+					new Square[][] {
+						{Square.Drain, Square.Drain},
+						{Square.Hold, Square.Hold},
+					},
+					new Pair[] {
+						new Pair(0, 1),
+					},
+				},
+				{
+					new Square[][] {
+						{Square.Drain, Square.Drain},
+						{Square.Drain, Square.Drain},
+					},
+					new Pair[] {
+						new Pair(0, 1),
+						new Pair(2, 3),
+						new Pair(0, 2),
+						new Pair(1, 3),
+					},
+				},
+				{
+					new Square[][] {
+						{Square.Drain, Square.Drain, Square.Drain},
+						{Square.Drain, Square.Drain, Square.Drain},
+						{Square.Drain, Square.Drain, Square.Drain},
+					},
+					new Pair[] {
+						new Pair(0, 1),
+						new Pair(1, 2),
+						new Pair(3, 4),
+						new Pair(4, 5),
+						new Pair(6, 7),
+						new Pair(7, 8),
+						new Pair(0, 3),
+						new Pair(1, 4),
+						new Pair(2, 5),
+						new Pair(3, 6),
+						new Pair(4, 7),
+						new Pair(5, 8),
+					},
+				},
+			});
+		}
+
+		@Parameterized.Parameter
+		public Square[][] squares;
+
+		@Parameterized.Parameter(1)
+		public Pair[] expected;
+
+		@Test
+		public void test() {
+			Square[] flattened = Arrays.stream(squares)
+				.flatMap(Arrays::stream)
+				.toArray(Square[]::new);
+
+			var result = new Vector<Pair>();
+			for (Pair pair : new Grid(flattened, squares.length)) {
+				result.add(pair);
+			}
+
+			assertArrayEquals(expected, result.toArray(Pair[]::new));
 		}
 	}
 }
